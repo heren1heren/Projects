c62e3e2b18634c9fbc11b51c27b00779dc67baa5
heren <heren1heren@gmail.com>
2023-11-14 13:53:42 -0700

    bout 4

--- library/1.js
+++ library/1.js
@@ -1,85 +1,70 @@
-const myLibrary = [book1  = {
-  title : 'hello',
-  author : 'hi',
-  pages : 99,
-  readYet : true,
-
-}, book2 = {
-  title : 'hobby',
-  author: 'lary',
-  pages: 100,
-  readYet: false,
-
-}, book3 = {
-  title: 'atomic habit',
-  author: 'goodGuy',
-  pages: 347,
-  readYet: true,
-
-}];
+const backdrop = document.getElementById('backdrop')
+const gridLibrary = document.getElementById('grid-library');
+const myLibrary = [];
 const popUp = document.getElementById('pop-up');
-const addButton = document.getElementById('');
-const submitButton = document.getElementById('');
+const addButton = document.getElementById('add-button');
+const submitButton = document.getElementById('submit-button');
 function Book(title,author,pages,readYet) {
      this.title = title;
      this.author = author;
      this.pages = pages;
      this.readYet = readYet;
 }
-function resetValue() {
-  title.value = ''; //? should write a separated function
-  author.value = '';
-  pages.value = 0; 
-  readYet.checked = false;    
-  submitButton.addeventlistener('click', () =>{
-     popUp.style.cssText = "display:none;"
-  })
 
-}
-function displayPopUp() {
-  popUp.classList.toggle('display') //? should toggle class instead
-}
-displayPopUp();
-function addBookToLibrary(book) {
-    addButton.addeventlistener('click',() => {  //? should write a separated function
- 
-       })
-    
-    
-     const title  =document.getElementById('').value;
-     const author = document.getElementById('').value;
-     const pages =  document.getElementById('').value;
-     const readYet= document.getElementById('').checked;
-      // after click submit button
-    const book = new Book(title,author,pages,readYet);
-  
-      myLibrary.push(book);
 
-    //reset values.
-    resetValue();
+function togglePopUp() {
+  popUp.classList.toggle('display'); //? should toggle class instead
+}
+function addBookToLibrary() {  
+  const title  =document.getElementById('title-input').value;
+  const author = document.getElementById('author-input').value;
+  const pages =  document.getElementById('pages-input').value;
+  const readYet= document.getElementById('readYet-input').checked;
+   const book = new Book(title,author,pages,readYet); 
+
+   myLibrary.push(book);
 }
 
 
-/** function for display book from myLibrary after click summit button or page load
- * 
- * 
- */
-const gridLibrary = document.getElementById('grid-library');
+addButton.addEventListener('click',() => {  
+  togglePopUp();  
+  backdrop.style.display = 'block';
+});
+submitButton.addEventListener('click', (event) =>{ // after click not thing happen
+ event.preventDefault();
+   togglePopUp();
+   backdrop.style.display = 'none';
+   
+  addBookToLibrary();
+ createCard(myLibrary[myLibrary.length - 1]);
+})
 
-function createCard(book) { //loop through every card in library with the corresponding book
+
+
+function createCard(book) { 
     const card = document.createElement('div');
     card.classList.add('card');
-    const status = () => this.readYet ? "have already read" : "haven't read"
+    const status =  book.readYet ? "Have already read" : "Haven't read"
     card.innerHTML = ` <header>Title</header>
     <main>
-        <div>Name:${this.title}  </div>
-      <div>Author:${this.author} </div>
-      <div>Pages: ${this.pages}</div>
+        <div>Name: ${book.title}  </div>
+      <div>Author: ${book.author} </div>
+      <div>Pages:  ${book.pages}</div>
     </main>
     
-        <button> ${status}</button> 
-        <button> Delete</button> `;
-        // if this.readYet is true display already read . else display haven't read
+        <button class="status-button"> ${status}</button> 
+        <button class="delete-button" > Delete</button> `;
+      gridLibrary.appendChild(card);
+}
+const readButton = document.getElementsByClassNam ('status-button');
+const deleteButton = document.getElementsByClassNa('delete-button')
+// function for switch state and change color of button status
+ function switchState() {
+
+ }
 
 
+//function to delete its card by set display:none;
+function deleteCard() {
+  
 }
