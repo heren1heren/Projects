93a53b549fe2400d601a83148146727a8c5df769
heren <heren1heren@gmail.com>
2023-11-14 15:02:33 -0700

    bout 5 lack of experence and still use AI

--- library/1.js
+++ library/1.js
@@ -13,13 +13,13 @@ function Book(title,author,pages,readYet) {
 
 
 function togglePopUp() {
-  popUp.classList.toggle('display'); //? should toggle class instead
+  popUp.classList.toggle('display'); 
 }
 function addBookToLibrary() {  
   const title  =document.getElementById('title-input').value;
   const author = document.getElementById('author-input').value;
   const pages =  document.getElementById('pages-input').value;
-  const readYet= document.getElementById('readYet-input').checked;
+  const readYet= document.getElementById('readYet-input').checked ? true : false;
    const book = new Book(title,author,pages,readYet); 
 
    myLibrary.push(book);
@@ -36,35 +36,88 @@ submitButton.addEventListener('click', (event) =>{ // after click not thing happ
    backdrop.style.display = 'none';
    
   addBookToLibrary();
- createCard(myLibrary[myLibrary.length - 1]);
+  const newIndex = myLibrary.length - 1;
+
+  createCard(myLibrary[newIndex], newIndex) // how to get index link with the card in here
 })
 
 
 
-function createCard(book) { 
+function createCard(book,index) { 
     const card = document.createElement('div');
+     card.dataset.index = index;
+     const cardIdex = card.dataset.index;
     card.classList.add('card');
     const status =  book.readYet ? "Have already read" : "Haven't read"
-    card.innerHTML = ` <header>Title</header>
+    card.innerHTML = ` <header>${book.title}</header>
     <main>
         <div>Name: ${book.title}  </div>
       <div>Author: ${book.author} </div>
       <div>Pages:  ${book.pages}</div>
-    </main>
+    </main>`;
     
-        <button class="status-button"> ${status}</button> 
-        <button class="delete-button" > Delete</button> `;
+    if (book.readYet) {
+      card.innerHTML  +=  `   <button class=" yesStatus status-button"> ${status}</button> 
+      <button class="delete-button" > Delete</button> `;
+
+    } else if(!book.readYet) {
+      card.innerHTML +=   `   <button class=" noStatus status-button"> ${status}</button> 
+      <button class="delete-button" > Delete</button> `;
+
+    }
+
+          
+        
       gridLibrary.appendChild(card);
 }
-const readButton = document.getElementsByClassNam ('status-button');
-const deleteButton = document.getElementsByClassNa('delete-button')
-// function for switch state and change color of button status
- function switchState() {
-
- }
 
 
-//function to delete its card by set display:none;
-function deleteCard() {
-  
+
+
+
+
+
+
+const readButton = document.getElementsByClassName ('status-button');
+const deleteButton = document.getElementsByClassName('delete-button')
+
+
+
+Book.prototype.toggleReadStatus = function() {
+ this.readYet = !this.readYet; 
+};
+function updateStatusButton(button, book) {
+  if (book.readYet) {
+    button.innerHTML = 'Have already read';
+    button.classList.add('yesStatus');
+    button.classList.remove('noStatus');
+  } else {
+    button.innerHTML = "Haven't read";
+    button.classList.add('noStatus');
+    button.classList.remove('yesStatus');
+  }
 }
+
+
+gridLibrary.addEventListener('click', (event) => {
+  const card = event.target.closest('.card');
+  if (card) {
+    const cardIndex = card.dataset.index;
+    const book = myLibrary[cardIndex];
+
+    if (event.target.classList.contains('status-button')) {
+      // Toggle the read status
+      book.toggleReadStatus();
+
+      // Update the status button appearance
+      updateStatusButton(event.target, book);
+    }
+
+    if (event.target.classList.contains('delete-button')) {
+      // Remove the card from the library and the DOM
+      myLibrary.splice(cardIndex, 1);
+      card.remove();
+    }
+  }
+});
+
